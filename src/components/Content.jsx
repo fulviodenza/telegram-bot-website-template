185e887f4983eb314d587e0f1f69414843068bd2
F <fulviodenza823@gmail.com>
2024-06-15 14:42:48 +0200

    Set margin x to mobile text

--- src/components/Content.jsx
+++ src/components/Content.jsx
@@ -4,8 +4,8 @@ import TelegramChat from './TelegramChat';
 function MainContent() {
   return (
       <div className="w-92 h-full flex-col my-10 flex items-center text-center gap-y-1">
-        <p className="text-xl">Seamlessly Integrate Telegram Notes into Your Notion Workspace</p>
-        <p className='text-gray-500'>Use your telegram client to write quick notes to your Second Brain</p>
+        <p className="text-xl mx-4 md:mx-0">Seamlessly Integrate Telegram Notes into Your Notion Workspace</p>
+        <p className='text-gray-500 mx-4 md:mx-0'>Use your telegram client to write quick notes to your Second Brain</p>
         <br/>
         <TelegramChat />
       </div>
